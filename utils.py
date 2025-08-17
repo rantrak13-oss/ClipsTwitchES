a532b85498d8b73be8f7d8d1e25e4e44453a18c3
rantrak13-oss <rantrak13@gmail.com>
2025-08-18 00:22:54 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+# utils.py
+import shutil
+import os
+
+def safe_remove_path(path):
+    try:
+        if os.path.isfile(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+    except Exception:
+        pass
